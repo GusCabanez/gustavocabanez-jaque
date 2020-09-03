038a39f811951bddef86d19e4a45cc1453e40e90
Gustavo Cabañez <guess724@hotmail.com>
2020-09-02 21:45:28 -0500

    Creacion de tabla con informacion de usuarios

--- src/app/@pages/users/user-list/user-list.component.ts
+++ src/app/@pages/users/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../services/users.service';
+import { User } from '../users.model';
 
 @Component({
   selector: 'app-user-list',
@@ -7,12 +8,14 @@ import { UsersService } from '../services/users.service';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  users: User[] = [];
 
   constructor( private usersService : UsersService ) { }
 
   ngOnInit(){
     this.usersService.getUsers().subscribe(data => {
-         console.log('From list component', data);
+      this.users = data.users;
+      console.log('Local users', this.users);
   });
 }
 
